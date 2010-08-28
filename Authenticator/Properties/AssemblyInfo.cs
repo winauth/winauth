95a92022176416491069cb5f6deab101df0ed027
Colin Mackie <cdm@legmail.com>
2010-08-28 19:25:00 +0000

    Version 1.0.1030

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1030.8")]
+[assembly: AssemblyVersion("1.0.1032.11")]
 #if !NETCF
-[assembly: AssemblyFileVersion("1.0.1030.8")]
+[assembly: AssemblyFileVersion("1.0.1032.11")]
 #endif
