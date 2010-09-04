6dfe55f655e80c45de9b808f84a7b9ff7e5ac730
Colin Mackie <cdm@legmail.com>
2010-09-04 23:33:24 +0000

    Build 1034.

--- WMAuth/Properties/AssemblyInfo.cs
+++ WMAuth/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1032.13")]
+[assembly: AssemblyVersion("1.0.1034.16")]
 
 // Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
 // as Device app does not support STA thread.
