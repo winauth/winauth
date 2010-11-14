d0d22b793e2f19da010ea080128aad168d6e53b2
Colin Mackie <cdm@legmail.com>
2010-11-14 03:52:41 +0000

    Update to 1.2.1045

--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.1043.30")]
-[assembly: AssemblyFileVersion("1.2.1043.30")]
+[assembly: AssemblyVersion("1.2.1045.33")]
+[assembly: AssemblyFileVersion("1.2.1045.33")]
