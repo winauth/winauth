2f6797c83690ca95d1d69ba8a642a4615073a888
Colin Mackie <cdm@legmail.com>
2011-01-25 02:46:36 +0000

    Update to 1.2.1050

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1049.35")]
+[assembly: AssemblyVersion("1.2.1050.38")]
 #if !NETCF
-[assembly: AssemblyFileVersion("1.2.1049.35")]
+[assembly: AssemblyFileVersion("1.2.1050.38")]
 #endif
--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.1049.36")]
-[assembly: AssemblyFileVersion("1.2.1049.36")]
+[assembly: AssemblyVersion("1.2.1050.39")]
+[assembly: AssemblyFileVersion("1.2.1050.39")]
