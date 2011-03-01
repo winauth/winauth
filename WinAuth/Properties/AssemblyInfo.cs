f1ce97c49000e375b0b504dd1c99d451270c115a
Colin Mackie <cdm@legmail.com>
2011-03-01 05:33:40 +0000

    Update to 1.4.1166

--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.1163.60")]
-[assembly: AssemblyFileVersion("1.4.1163.60")]
+[assembly: AssemblyVersion("1.4.1166.66")]
+[assembly: AssemblyFileVersion("1.4.1166.66")]
