14e1fcfe261288ae8d772c6c80a36fd946487a1c
Colin Mackie <cdm@legmail.com>
2011-08-03 17:47:51 +0000

    Build 1.5.113.86

--- WMAuth/Properties/AssemblyInfo.cs
+++ WMAuth/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.111.85")]
+[assembly: AssemblyVersion("1.3.113.88")]
 
 // Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
 // as Device app does not support STA thread.
--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.111.84")]
-[assembly: AssemblyFileVersion("1.5.111.84")]
+[assembly: AssemblyVersion("1.5.113.87")]
+[assembly: AssemblyFileVersion("1.5.113.87")]
