3b716472abd598195e59941b51ccc78f275a6346
Colin Mackie <cdm@legmail.com>
2011-08-07 16:58:44 +0000

    Update to 1.5.1111

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.119.92")]
+[assembly: AssemblyVersion("1.5.1111.95")]
 #if !NETCF
-[assembly: AssemblyFileVersion("1.5.119.92")]
+[assembly: AssemblyFileVersion("1.5.1111.95")]
 #endif
--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.119.93")]
-[assembly: AssemblyFileVersion("1.5.119.93")]
+[assembly: AssemblyVersion("1.5.1111.96")]
+[assembly: AssemblyFileVersion("1.5.1111.96")]
