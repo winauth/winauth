bbb9573a708017dbb5e15957e51f91d91fe9b454
WinAuth <winauth@gmail.com>
2012-05-31 21:20:02 +0000

    Update to 1.7

--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.1122.105")]
-[assembly: AssemblyFileVersion("1.6.1122.105")]
+[assembly: AssemblyVersion("1.7.1122.105")]
+[assembly: AssemblyFileVersion("1.7.1122.105")]
