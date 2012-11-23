539eec261fe16a9ae6232a56505bd5660b8b4862
WinAuth <winauth@gmail.com>
2012-11-22 23:34:12 -0800

    Update to 2.0.6.176

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.6.174")]
+[assembly: AssemblyVersion("2.0.6.176")]
 #if !NETCF
-[assembly: AssemblyFileVersion("2.0.6.174")]
+[assembly: AssemblyFileVersion("2.0.6.176")]
 #endif
