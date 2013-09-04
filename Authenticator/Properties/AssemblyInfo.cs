295f6e01d0397f3991a6aacde393180092813d5a
Colin Mackie <winauth@gmail.com>
2013-09-04 08:30:02 -0700

    Update to 3.0.14

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.12.0")]
+[assembly: AssemblyVersion("3.0.14.0")]
 #if !NETCF
-[assembly: AssemblyFileVersion("3.0.12.0")]
+[assembly: AssemblyFileVersion("3.0.14.0")]
 #endif
