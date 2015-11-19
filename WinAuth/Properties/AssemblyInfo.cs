9efd3aac33535a5295a96e02816f2e093d6cbb4c
Colin Mackie <winauth@gmail.com>
2015-11-18 22:53:05 -0800

    Update to 3.3.5

--- WinAuth/Properties/AssemblyInfo.cs
+++ WinAuth/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.3.8")]
-[assembly: AssemblyFileVersion("3.3.3.8")]
+[assembly: AssemblyVersion("3.3.5.2")]
+[assembly: AssemblyFileVersion("3.3.5.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
